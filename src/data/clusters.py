390d49052674f4aaa7b21641c052c4c33162980c
Will Langdale <will.langdale@trade.gov.uk>
2023-09-06 15:22:23 +0000

    Fixed cluster class

--- src/data/clusters.py
+++ src/data/clusters.py
@@ -1,4 +1,5 @@
 from src.data import utils as du
+import uuid
 
 
 class Clusters(object):
@@ -11,6 +12,7 @@ class Clusters(object):
         * schema: the cluster table's schema name
         * table: the cluster table's table name
         * schema_table: the cluster table's full name
+        * star: an object of class Star that wraps the star table
 
     Methods:
         * create(dim=None, overwrite): Drops all data and recreates the cluster
@@ -22,10 +24,11 @@ class Clusters(object):
         with the requested fields attached
     """
 
-    def __init__(self, schema: str, table: str):
+    def __init__(self, schema: str, table: str, star: object):
         self.schema = schema
         self.table = table
         self.schema_table = f'"{self.schema}"."{self.table}"'
+        self.star = star
 
     def create(self, overwrite: bool, dim: int = None):
         """
@@ -48,18 +51,38 @@ class Clusters(object):
         sql = f"""
             {drop}
             create table {exist_clause} {self.schema_table} (
-                uuid uuid
-                cluster uuid
-                id text
-                source int
-                n int
+                uuid uuid primary key,
+                cluster uuid not null,
+                id text not null,
+                source int not null,
+                n int not null
             );
         """
 
         du.query_nonreturn(sql)
 
         if dim is not None:
-            pass
+            dim_table = self.star.get(star_id=dim, response="dim")
+
+            sql = f"""
+                select
+                    id
+                from
+                    {dim_table}
+            """
+
+            to_insert = du.query(sql)
+            to_insert["uuid"] = [uuid.uuid4() for _ in range(len(to_insert.index))]
+            to_insert["cluster"] = [uuid.uuid4() for _ in range(len(to_insert.index))]
+            to_insert["source"] = dim
+            to_insert["n"] = 1
+
+            du.data_workspace_write(
+                df=to_insert, schema=self.schema, table=self.table, if_exists="append"
+            )
+
+    def read(self):
+        return du.dataset(self.schema_table)
 
     def get_data(self, fields: list):
         """
