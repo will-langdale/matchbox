ac337825080fc467777ee465f86d82e2e765a148
Will Langdale <will.langdale@trade.gov.uk>
2023-09-06 16:11:47 +0000

    Finished initial tranch of data classes

--- src/data/clusters.py
+++ src/data/clusters.py
@@ -18,8 +18,8 @@ class Clusters(object):
         * create(dim=None, overwrite): Drops all data and recreates the cluster
         table. If a dimension table is specified, adds each row as a new cluster
         * read(): Returns the cluster table
-        * add_clusters(lookup): Using a lookup table, adds new entries
-        to the cluster table
+        * add_clusters(probabilities): Using a probabilities table, adds new
+        entries to the cluster table
         * get_data(fields): returns the cluster table pivoted wide,
         with the requested fields attached
     """
@@ -83,6 +83,26 @@ class Clusters(object):
 
     def read(self):
         return du.dataset(self.schema_table)
+
+    def add_clusters(self, probabilities):
+        """
+        The core probabilities > clusters algorithm, as proposed in the
+        v0.2 output currently described in the README.
+
+        1. Order probabilities from high to low probability
+        2. Take the highest of each unique pair and add to cluster table
+        3. Remove all members of matched pairs from either side of
+        probabilities
+        4. Repeat 2-3 until lookup is empty
+
+        This algorithm should both work with one step in an additive
+        pattern, or a big group of matches made concurrently.
+
+        Instinctively, I think this should read the Postgres table but
+        perform the algorithm in memory. We'll see.
+        """
+        # TODO: implement once we've populated a probabilities table
+        pass
 
     def get_data(self, fields: list):
         """
