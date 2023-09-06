c25d8000d4691ce9b10a23c4ba580212916ed3d3
Will Langdale <will.langdale@trade.gov.uk>
2023-09-06 13:52:54 +0000

    Created Star class with get functionality to wrap table

--- src/data/clusters.py
+++ src/data/clusters.py
@@ -1,16 +1,79 @@
-def clusters_to_data(cluster_table: str, fields: list):
+from src.data import utils as du
+
+
+class Clusters(object):
     """
-    Build the cluster table at point n in the 🐙blocktopus build process.
+    A class to interact with the company matching framework's clusters table.
+    Enforces things are written in the right shape, and facilates easy
+    retrieval of data in various shapes.
 
-    Arguments:
-        cluster_table: The location of the cluster table
-        fields: The data to retrieve from the cluster's dimension tables
+    Attributes:
+        * schema: the cluster table's schema name
+        * table: the cluster table's table name
+        * schema_table: the cluster table's full name
 
-    Returns:
-        A dataframe with one row per company entity, and one column per
-        requested field
+    Methods:
+        * create(dim=None, overwrite): Drops all data and recreates the cluster
+        table. If a dimension table is specified, adds each row as a new cluster
+        * read(): Returns the cluster table
+        * add_clusters(lookup): Using a lookup table, adds new entries
+        to the cluster table
+        * get_data(fields): returns the cluster table pivoted wide,
+        with the requested fields attached
     """
 
-    data = None
+    def __init__(self, schema: str, table: str):
+        self.schema = schema
+        self.table = table
+        self.schema_table = f'"{self.schema}"."{self.table}"'
 
-    return data
+    def create(self, overwrite: bool, dim: int = None):
+        """
+        Creates a new cluster table. If a dimension table is specified, adds
+        each row as a new cluster to the recreated table.
+
+        Arguments:
+            dim: [Optional] The STAR ID of a dimension table to populate the
+            new cluster table with
+            overwrite: Whether or not to overwrite an existing cluster table
+        """
+
+        if overwrite:
+            drop = f"drop table if exists {self.schema_table};"
+            exist_clause = ""
+        else:
+            drop = ""
+            exist_clause = "if not exists"
+
+        sql = f"""
+            {drop}
+            create table {exist_clause} {self.schema_table} (
+                uuid uuid
+                cluster uuid
+                id text
+                source int
+                n int
+            );
+        """
+
+        du.query_nonreturn(sql)
+
+        if dim is not None:
+            pass
+
+    def get_data(self, fields: list):
+        """
+        Build the cluster table at point n in the 🐙blocktopus build process.
+
+        Arguments:
+            cluster_table: The location of the cluster table
+            fields: The data to retrieve from the cluster's dimension tables
+
+        Returns:
+            A dataframe with one row per company entity, and one column per
+            requested field
+        """
+
+        data = None
+
+        return data
--- src/data/star.py
+++ src/data/star.py
@@ -1,5 +1,5 @@
 from src.data import utils as du
-from src.config import link_pipeline
+from src.config import link_pipeline as link_pipeline_dict
 
 import logging
 from dotenv import load_dotenv, find_dotenv
@@ -8,57 +8,152 @@ from hashlib import sha256
 import pandas as pd
 
 
-def make_star_table(
-    link_pipeline: dict, star_schema: str, star_table: str, overwrite: bool
-):
+class Star(object):
     """
-    Creates a STAR lookup table from a link_pipeline settings object.
+    A class to interact with the company matching framework's STAR table.
+    Enforces things are written in the right shape, and facilates easy
+    retrieval of data in various shapes.
 
-    Arguments:
-        link_pipeline: The source link_pipeline settings object
-        star_schema: The target STAR lookup schema
-        star_table: The target STAR lookup table
-        overwrite: Whether to overwrite the target if it exists
+    Attributes:
+        * schema: the cluster table's schema name
+        * table: the cluster table's table name
+        * schema_table: the cluster table's full name
 
-    Raises:
-        ValueError
-            * If the table exists and overwrite is False
-            * If the hash function fails to produce unique keys
+    Methods:
+        * create(link_pipeline, overwrite): Drops all data and recreates the
+        star table from the supplied link_pipeline
+        * read(): Returns the STAR table
+        * get(star_id, fact, dim, response): For strings of ID, fact, dimension,
+        some, both or all, returns the requested datatype if a single match can
+        be made
     """
 
-    if_exists = "replace" if overwrite else "fail"
-    schema_and_table = f'"{os.getenv("SCHEMA")}"."{os.getenv("STAR_TABLE")}"'
+    def __init__(self, schema: str, table: str):
+        self.schema = schema
+        self.table = table
+        self.schema_table = f'"{self.schema}"."{self.table}"'
 
-    if du.check_table_exists(schema_and_table) and not overwrite:
-        raise ValueError("Table exists. Set overwrite to True if you want to proceed.")
+    def create(self, link_pipeline: dict, overwrite: bool):
+        """
+        Creates a STAR lookup table from a link_pipeline settings object.
 
-    out = {"id": [], "fact": [], "dim": []}
-    for table in link_pipeline:
-        fact = link_pipeline[table]["fact"]
-        dim = link_pipeline[table]["dim"]
-        hash_hex = sha256(
-            f"{fact}{dim}".encode(encoding="UTF-8", errors="strict")
-        ).hexdigest()
-        # We need a hash that:
-        # * Is an integer
-        # * Is stable
-        # * Is unique for the amount of dims we'll ever see
-        # I therefore manipulate the hex to 0-65535 to fit in a 16-bit unsigned
-        # int field
-        hash_int = int(hash_hex, 16) % 65536
+        Arguments:
+            link_pipeline: The source link_pipeline settings object
+            overwrite: Whether to overwrite the target if it exists
 
-        out["id"].append(hash_int)
-        out["fact"].append(fact)
-        out["dim"].append(dim)
+        Raises:
+            ValueError
+                * If the table exists and overwrite is False
+                * If the hash function fails to produce unique keys
+        """
 
-    star = pd.DataFrame.from_dict(out)
+        if_exists = "replace" if overwrite else "fail"
 
-    if len(star) != len(star.id.unique()):
-        raise ValueError("Hash function has failed to produce unique keys. Change it.")
+        if du.check_table_exists(self.schema_table) and not overwrite:
+            raise ValueError(
+                """
+                Table exists. Set overwrite to True if you want to proceed.
+            """
+            )
 
-    du.data_workspace_write(
-        df=star, schema=star_schema, table=star_table, if_exists=if_exists
-    )
+        out = {"id": [], "fact": [], "dim": []}
+        for table in link_pipeline:
+            fact = link_pipeline[table]["fact"]
+            dim = link_pipeline[table]["dim"]
+            hash_hex = sha256(
+                f"{fact}{dim}".encode(encoding="UTF-8", errors="strict")
+            ).hexdigest()
+            # We need a hash that:
+            # * Is an integer
+            # * Is stable
+            # * Is unique for the amount of dims we'll ever see
+            # I therefore manipulate the hex to 0-65535 to fit in a 16-bit unsigned
+            # int field
+            hash_int = int(hash_hex, 16) % 65536
+
+            out["id"].append(hash_int)
+            out["fact"].append(fact)
+            out["dim"].append(dim)
+
+        star = pd.DataFrame.from_dict(out)
+
+        if len(star) != len(star.id.unique()):
+            raise ValueError(
+                """
+                Hash function has failed to produce unique keys. Change it.
+            """
+            )
+
+        du.data_workspace_write(
+            df=star, schema=self.schema, table=self.table, if_exists=if_exists
+        )
+
+    def read(self):
+        return du.dataset(self.schema_table)
+
+    def get(
+        self, star_id: int = None, fact: str = None, dim: str = None, response="id"
+    ):
+        """
+        For strings of ID, fact, dimension, some, both or all, returns the
+        requested datatype if a single match can be made.
+
+        Arguments:
+            star_id: An integer in the ID column
+            fact: A string found in the fact column
+            dim: A string found in the dim column
+            response: Returned object. One of 'id', 'fact' or 'dim'
+
+        Raises:
+            ValueError:
+                * If response argument not one of 'id', 'fact' or 'dim'
+                * If one of star_id, fact or dim isn't given
+                * If more than one value is returned
+
+        Returns:
+            Integer of ID, or string of dimension or fact table
+        """
+        if response not in ["id", "fact", "dim"]:
+            raise ValueError(
+                """
+                Response argument not one of 'id', 'fact' or 'dim'
+            """
+            )
+        if all(i is None for i in [star_id, fact, dim]):
+            raise ValueError(
+                """
+                Must supply at least one argument to star_id, fact or dim
+            """
+            )
+
+        where_id = f"id = {star_id}" if star_id is not None else ""
+        where_fact = f"fact = '{fact}'" if fact is not None else ""
+        where_dim = f"dim = '{dim}'" if dim is not None else ""
+        where_clase = " and ".join(
+            filter(lambda i: (i != ""), [where_id, where_fact, where_dim])
+        )
+
+        sql = f"""
+            select
+                {response}
+            from
+                {self.schema_table}
+            where
+                {where_clase}
+        """
+
+        filtered_star = du.query(sql)
+
+        if len(filtered_star) > 1:
+            raise ValueError(
+                """
+                More than one value returned. Refine your request.
+            """
+            )
+
+        results = filtered_star[response].values.tolist()
+
+        return results[0]
 
 
 if __name__ == "__main__":
@@ -74,10 +169,10 @@ if __name__ == "__main__":
         f'Creating STAR table "{os.getenv("SCHEMA")}"."{os.getenv("STAR_TABLE")}"'
     )
 
-    make_star_table(
-        link_pipeline=link_pipeline,
-        star_schema=os.getenv("SCHEMA"),
-        star_table=os.getenv("STAR_TABLE"),
+    star = Star(schema=os.getenv("SCHEMA"), table=os.getenv("STAR_TABLE"))
+
+    star.create(
+        link_pipeline=link_pipeline_dict,
         overwrite=True,
     )
 
