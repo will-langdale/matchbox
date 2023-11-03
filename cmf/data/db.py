67d49fcbe6c89b87f51683ee1c95f42d9c6a6bad
Will Langdale <will.langdale@trade.gov.uk>
2023-11-03 07:21:08 +0000

    Initial refactor

---
+++ cmf/data/db.py
@@ -0,0 +1,139 @@
+from cmf.data import utils as du
+from cmf.data.models import Table
+from cmf.data.datasets import Dataset
+from cmf.config import link_pipeline
+
+import logging
+from dotenv import load_dotenv, find_dotenv
+import os
+from hashlib import sha256
+import pandas as pd
+from pydantic import BaseModel, computed_field, field_validator
+from sqlalchemy.sql import text as sql_text
+from typing import Dict
+
+
+class DB(BaseModel):
+    """
+    The entrypoint to the whole Company Matching Framework database.
+    """
+
+    db_table: Table
+
+    @field_validator("db_table")
+    @classmethod
+    def check_star(cls, v: Table) -> Table:
+        star_fields = {"id", "fact", "dim"}
+        assert set(v.db_fields) == star_fields
+        return v
+
+    @computed_field
+    def db_datasets(self) -> Dict[str, Table]:
+        sql = f"""
+            select id, fact, dim
+            from {self.db_table.db_schema_table};
+        """
+
+        db_datasets = {}
+
+        with du.sql_engine.connect() as connection:
+            res = connection.execute(sql_text(sql))
+            for row in res:
+                fact_schema, fact_table = du.get_schema_table_names(
+                    row.fact, validate=True
+                )
+                dim_schema, dim_table = du.get_schema_table_names(
+                    row.dim, validate=True
+                )
+                fact = Table(db_schema=fact_schema, db_table=fact_table)
+                dim = Table(db_schema=dim_schema, db_table=dim_table)
+                dataset = Dataset(db_fact=fact, db_dim=dim)
+
+                db_datasets[dataset.db_dim.db_schema_table] = dataset
+                db_datasets[dataset.db_fact.db_schema_table] = dataset
+                db_datasets[dataset.db_id] = dataset
+
+        return db_datasets
+
+    def create(self, link_pipeline: dict, overwrite: bool):
+        """
+        Creates a STAR lookup table from a link_pipeline settings object.
+
+        Arguments:
+            link_pipeline: The source link_pipeline settings object
+            overwrite: Whether to overwrite the target if it exists
+
+        Raises:
+            ValueError
+                * If the table exists and overwrite is False
+                * If the hash function fails to produce unique keys
+        """
+
+        if_exists = "replace" if overwrite else "fail"
+
+        if self.table.exists and not overwrite:
+            raise ValueError(
+                """
+                Table exists. Set overwrite to True if you want to proceed.
+            """
+            )
+
+        out = {"id": [], "fact": [], "dim": []}
+        for table in link_pipeline:
+            fact = link_pipeline[table]["fact"]
+            dim = link_pipeline[table]["dim"]
+            hash_hex = sha256(
+                f"{fact}{dim}".encode(encoding="UTF-8", errors="strict")
+            ).hexdigest()
+            # We need a hash that:
+            # * Is an integer
+            # * Is stable
+            # * Is unique for the amount of dims we'll ever see
+            # I therefore manipulate the hex to 0-65535 to fit in a 16-bit unsigned
+            # int field
+            hash_int = int(hash_hex, 16) % 65536
+
+            out["id"].append(hash_int)
+            out["fact"].append(fact)
+            out["dim"].append(dim)
+
+        star = pd.DataFrame.from_dict(out)
+
+        if len(star) != len(star.id.unique()):
+            raise ValueError(
+                """
+                Hash function has failed to produce unique keys. Change it.
+            """
+            )
+
+        du.data_workspace_write(
+            df=star,
+            schema=self.table.db_schema,
+            table=self.table.db_table,
+            if_exists=if_exists,
+        )
+
+
+if __name__ == "__main__":
+    dotenv_path = find_dotenv()
+    load_dotenv(dotenv_path)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=du.LOG_FMT,
+    )
+    logger = logging.getLogger(__name__)
+    logger.info(
+        f'Creating STAR table "{os.getenv("SCHEMA")}"."{os.getenv("STAR_TABLE")}"'
+    )
+
+    db = DB(
+        db_table=Table(db_schema=os.getenv("SCHEMA"), db_table=os.getenv("STAR_TABLE"))
+    )
+
+    db.create(
+        link_pipeline=link_pipeline,
+        overwrite=True,
+    )
+
+    logger.info("Written STAR table")
---
+++ cmf/data/models.py
@@ -0,0 +1,87 @@
+from cmf.data import utils as du
+
+from typing import List, Optional
+from pydantic import BaseModel, computed_field, field_validator, model_validator
+from sqlalchemy.sql import text as sql_text
+from sqlalchemy.exc import MultipleResultsFound
+from pandas import DataFrame
+
+
+class Table(BaseModel):
+    db_schema: str
+    db_table: str
+    db_fields: Optional[List[str]] = None
+
+    @field_validator("db_schema", "db_table")
+    @classmethod
+    def unquote(cls, v: str) -> str:
+        return v.replace('"', "")
+
+    @computed_field
+    def db_schema_table(self) -> str:
+        return f"{self.db_schema}.{self.db_table}"
+
+    @computed_field
+    def exists(self) -> bool:
+        sql = f"""
+            select exists (
+                select from information_schema.tables
+                where table_schema = '{self.db_schema}'
+                and table_name = '{self.db_table}'
+            );
+        """
+
+        with du.sql_engine.connect() as connection:
+            res = connection.execute(sql_text(sql))
+
+            try:
+                exists = res.scalar()
+            except MultipleResultsFound:
+                raise ValueError(
+                    "Multiple results found. Table or schema name unclear."
+                )
+
+        return exists
+
+    @model_validator(mode="after")
+    def get_db_fields(self) -> "Table":
+        if self.exists:
+            with du.sql_engine.connect() as connection:
+                res = connection.execute(
+                    sql_text(f"select * from {self.db_schema_table} limit 0")
+                )
+            self.db_fields = list(res._metadata.keys)
+        return self
+
+    def read(self, select: list = None, sample: float = None) -> DataFrame:
+        """
+        Returns the table as pandas dataframe.
+
+        Arguments:
+            select: [optional] a list of columns to select. Aliasing
+            and casting permitted
+            sample:[optional] the percentage sample to return. Used to
+            speed up debugging of downstream processes
+        """
+        fields = "*" if select is None else " ,".join(select)
+        if sample is not None:
+            sample_clause = f"tablesample system ({sample})"
+        else:
+            sample_clause = ""
+
+        return du.query(
+            f"""
+            select
+                {fields}
+            from
+                {self.db_schema_table} {sample_clause};
+        """
+        )
+
+
+if __name__ == "__main__":
+    # y = Table(db_schema="_user_eaf4fd9a", db_table="cm_test")
+    # print(y)
+    # x = Table(db_schema="_user_eaf4fd9a", db_table="cm_star")
+    # print(x)
+    pass
