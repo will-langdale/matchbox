77c4a3b246faf652ca92c898095ad3fa12b2f352
Will Langdale <will.langdale@trade.gov.uk>
2024-02-29 15:41:49 +0000

    Trying to force schema presence and database initialisation on import

--- cmf/__init__.py
+++ cmf/__init__.py
@@ -1,3 +1,5 @@
+import os.environ
+
 from cmf.data.results import to_clusters
 from cmf.dedupers.make_deduper import make_deduper
 from cmf.helpers.cleaner import process
@@ -5,3 +7,6 @@ from cmf.helpers.selector import query
 from cmf.linkers.make_linker import make_linker
 
 __all__ = ("make_deduper", "make_linker", "to_clusters", "process", "query")
+
+if "SCHEMA" not in os.environ:
+    raise KeyError("SCHEMA environment variable not set.")
