1f549d08de7ac6735add440c1b7448e6233e0468
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2024-12-07 19:44:52 +0000

    Generalise vcr cassettes path

--- test/client/test_handler.py
+++ test/client/test_handler.py
@@ -1,10 +1,16 @@
 from functools import wraps
+from os import path
+from pathlib import Path
 
 from matchbox.client._handler import get_resolution_graph
 from vcr import use_cassette
 
 
 def vcr_cassette(func):
+    test_dir = Path(__file__).resolve().parents[1]
+    cassette_name = f"{func.__name__}.yaml"
+    path.join(test_dir, "fixtures", cassette_name)
+
     @wraps(func)
     @use_cassette(f"test/fixtures/vcr_cassettes/{func.__name__}.yaml")
     def wrapper(*args, **kwargs):
