9ceec9393ce3acd62a9f5fc3163efd148f74c7c9
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2024-12-11 10:53:35 +0000

    Small PR review fixes

--- test/client/test_handler.py
+++ test/client/test_handler.py
@@ -1,18 +1,17 @@
 from functools import wraps
-from os import path
 from pathlib import Path
+from typing import Callable
 
 from matchbox.client._handler import get_resolution_graph
 from vcr import use_cassette
 
 
-def vcr_cassette(func):
+def vcr_cassette(func: Callable) -> Callable:
     test_dir = Path(__file__).resolve().parents[1]
     cassette_name = f"{func.__name__}.yaml"
-    path.join(test_dir, "fixtures", cassette_name)
 
     @wraps(func)
-    @use_cassette(f"test/fixtures/vcr_cassettes/{func.__name__}.yaml")
+    @use_cassette(test_dir / "fixtures" / "vcr_cassettes" / cassette_name)
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
 
