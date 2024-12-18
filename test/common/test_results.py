a91a2f5080a084fbd849121c3b3536f3905b64d8
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2024-12-18 12:22:17 +0000

    Unit test disjoint sets

---
+++ test/common/test_results.py
@@ -0,0 +1,40 @@
+from matchbox.common.results import DisjointSet
+
+
+class TestDisjointSet:
+    def test_disjoint_set_empty(self):
+        dsj = DisjointSet()
+
+        assert dsj.get_components() == []
+
+    def test_disjoint_set_same(self):
+        dsj = DisjointSet()
+        dsj.union(1, 1)
+
+        assert dsj.get_components() == [{1}]
+
+    def test_disjoint_set_redundant(self):
+        dsj = DisjointSet()
+        dsj.union(1, 2)
+        dsj.union(2, 1)
+
+        assert dsj.get_components() == [{1, 2}]
+
+    def test_disjoint_set_split(self):
+        dsj = DisjointSet()
+        dsj.union(1, 2)
+        dsj.union(3, 4)
+        dsj.union(5, 6)
+
+        assert sorted(dsj.get_components()) == [{1, 2}, {3, 4}, {5, 6}]
+
+    def test_disjoint_set_merged(self):
+        dsj = DisjointSet()
+        dsj.union(1, 2)
+        dsj.union(3, 4)
+        dsj.union(5, 6)
+
+        dsj.union(2, 3)
+        dsj.union(4, 5)
+
+        assert dsj.get_components() == [{1, 2, 3, 4, 5, 6}]
