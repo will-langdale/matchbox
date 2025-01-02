e474e1a069a72d125d70503a5f4c1f899956257a
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2025-01-02 15:00:01 +0000

    Test generation of benchmark tables

--- test/server/test_postgresql.py
+++ test/server/test_postgresql.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import pytest
 import rustworkx as rx
 from pandas import DataFrame
@@ -9,6 +11,7 @@ from matchbox.client.results import (
     ModelType,
     ProbabilityResults,
 )
+from matchbox.server.postgresql.benchmark.generate_tables import generate_all_tables
 from matchbox.server.postgresql.benchmark.init_schema import create_tables, empty_schema
 from matchbox.server.postgresql.db import MBDB
 from matchbox.server.postgresql.utils.insert import _cluster_results_to_hierarchical
@@ -219,10 +222,40 @@ def test_benchmark_init_schema():
 
     with MBDB.get_engine().connect() as con:
         con.execute(text(empty_schema()))
+        con.commit()
         n_tables = int(con.execute(count_tables).scalar())
         assert n_tables == 0
 
         con.execute(text(create_tables()))
+        con.commit()
         n_tables_expected = len(MBDB.MatchboxBase.metadata.tables)
         n_tables = int(con.execute(count_tables).scalar())
         assert n_tables == n_tables_expected
+
+
+def test_benchmark_generate_tables():
+    schema = MBDB.MatchboxBase.metadata.schema
+
+    def array_encode(array: Iterable[str]):
+        if not array:
+            return None
+        escaped_l = [f'"{s}"' for s in array]
+        list_rep = ", ".join(escaped_l)
+        return "{" + list_rep + "}"
+
+    with MBDB.get_engine().connect() as con:
+        con.execute(text(empty_schema()))
+        con.commit()
+
+        results = generate_all_tables(20, 5, 15, 5, 30)
+
+        for table_name, table_arrow in results.items():
+            df = table_arrow.to_pandas()
+            # Pandas' `to_sql` dislikes arrays
+            if "source_pk" in df.columns:
+                df["source_pk"] = df["source_pk"].apply(array_encode)
+            # Pandas' `to_sql` dislikes large unsigned ints
+            for c in df.columns:
+                if df[c].dtype == "uint64":
+                    df[c] = df[c].astype("int64")
+            df.to_sql(table_name, con, schema)
