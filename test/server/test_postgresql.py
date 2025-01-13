9ce696254377577555b0b01812140920484b50d2
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2025-01-13 12:19:03 +0000

    Restore unit tests

--- test/server/test_postgresql.py
+++ test/server/test_postgresql.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Iterable
 
 import pandas as pd
 import pyarrow as pa
@@ -7,6 +7,11 @@ from sqlalchemy import text
 
 from matchbox.common.db import Source
 from matchbox.server.postgresql import MatchboxPostgres
+from matchbox.server.postgresql.benchmark.generate_tables import (
+    generate_all_tables,
+    generate_result_tables,
+)
+from matchbox.server.postgresql.benchmark.init_schema import create_tables, empty_schema
 from matchbox.server.postgresql.benchmark.query import compile_query_sql
 from matchbox.server.postgresql.db import MBDB
 from matchbox.server.postgresql.utils.insert import HashIDMap
@@ -111,3 +116,54 @@ def test_benchmark_query_generation(
 
     assert df.id.nunique() == parameters["unique_ids"]
     assert df.pk.nunique() == parameters["unique_pks"]
+
+
+@pytest.mark.parametrize(
+    "left_ids, right_ids, next_id, n_components, n_probs",
+    (
+        [range(10_000), None, 10_000, 8000, 2000],
+        [range(8000), range(8000, 16_000), 16_000, 6000, 10_000],
+    ),
+    ids=["dedupe", "link"],
+)
+def test_benchmark_result_tables(left_ids, right_ids, next_id, n_components, n_probs):
+    resolution_id = None
+
+    (top_clusters, _, _, _, _) = generate_result_tables(
+        left_ids, right_ids, resolution_id, next_id, n_components, n_probs
+    )
+
+    assert len(top_clusters) == n_components
+
+
+def test_benchmark_generate_tables():
+    schema = MBDB.MatchboxBase.metadata.schema
+
+    def array_encode(array: Iterable[str]):
+        if not array:
+            return None
+        escaped_l = [f'"{s}"' for s in array]
+        list_rep = ", ".join(escaped_l)
+        return "{" + list_rep + "}"
+
+    with MBDB.get_engine().connect() as con:
+        con.execute(text(empty_schema()))
+        con.execute(text(create_tables()))
+        con.commit()
+
+        results = generate_all_tables(20, 5, 25, 5, 25)
+
+        assert len(results) == len(MBDB.MatchboxBase.metadata.tables)
+
+        for table_name, table_arrow in results.items():
+            df = table_arrow.to_pandas()
+            # Pandas' `to_sql` dislikes arrays
+            if "source_pk" in df.columns:
+                df["source_pk"] = df["source_pk"].apply(array_encode)
+            # Pandas' `to_sql` dislikes large unsigned ints
+            for c in df.columns:
+                if df[c].dtype == "uint64":
+                    df[c] = df[c].astype("int64")
+            df.to_sql(
+                name=table_name, con=con, schema=schema, index=False, if_exists="append"
+            )
