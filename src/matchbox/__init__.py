231db1ba8301e514d3e376da5c3146839f5003b9
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2025-01-23 12:53:37 +0000

    Remove legacy function from top namespce

--- src/matchbox/__init__.py
+++ src/matchbox/__init__.py
@@ -5,7 +5,7 @@ from matchbox.client.helpers.index import index
 from matchbox.client.helpers.selector import match, query
 from matchbox.client.models.models import make_model
 
-__all__ = ("make_model", "to_clusters", "process", "query", "match", "index")
+__all__ = ("make_model", "process", "query", "match", "index")
 
 dotenv_path = find_dotenv(usecwd=True)
 load_dotenv(dotenv_path)
