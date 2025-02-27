8bdbf66d69d394a0aa13917212cd3927cfe805f5
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2025-02-27 10:25:53 +0000

    Surface all client names to the top-level

--- src/matchbox/__init__.py
+++ src/matchbox/__init__.py
@@ -4,9 +4,4 @@ dotenv_path = find_dotenv(usecwd=True)
 load_dotenv(dotenv_path)
 
 # Environment variables must be loaded first for other imports to work
-from matchbox.client import (  # noqa: E402
-    match,
-    query,
-)
-
-__all__ = ("query", "match")
+from matchbox.client import *  # noqa: E402, F403
