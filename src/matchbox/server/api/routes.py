f23f1bcd7ba60638ccddaedef2925853ae4225e7
Will Langdale <will.langdale@businessandtrade.gov.uk>
2025-04-22 17:16:18 +0100

    Re-add ASIM formatter.

--- src/matchbox/server/api/routes.py
+++ src/matchbox/server/api/routes.py
@@ -42,6 +42,7 @@ from matchbox.common.exceptions import (
     MatchboxSourceNotFoundError,
 )
 from matchbox.common.graph import ResolutionGraph
+from matchbox.common.logging import ASIMFormatter
 from matchbox.common.sources import Match, Source, SourceAddress
 from matchbox.server.api.arrow import table_to_s3
 from matchbox.server.api.cache import MetadataStore, process_upload
@@ -77,12 +78,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     backend = settings_to_backend(settings)
 
     # Define common formatter
-    formatter = logging.Formatter("[%(name)s %(levelname)s] %(message)s")
-
-    # Configure handler
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(backend.settings.log_level)
-    handler.setFormatter(formatter)
+    formatter = ASIMFormatter()
 
     # Configure loggers with the same handler and formatter
     loggers_to_configure = [
@@ -95,6 +91,11 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     ]
 
     for logger_name in loggers_to_configure:
+        # Configure handler
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(backend.settings.log_level)
+        handler.setFormatter(formatter)
+
         logger = logging.getLogger(logger_name)
         logger.setLevel(backend.settings.log_level)
         # Remove any existing handlers to avoid duplicates
