e4635d16f653725b66e118d1c6ac9b01c79fcf31
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2025-05-22 17:14:04 +0100

    Re-order tests

--- test/common/test_sources.py
+++ test/common/test_sources.py
@@ -628,46 +628,6 @@ def test_source_hash_data_null_identifier(mock_query: Mock, sqlite_warehouse: En
         source.hash_data()
 
 
-# Match
-
-
-def test_match_validates():
-    """Match objects are validated when they're instantiated."""
-    Match(
-        cluster=1,
-        source="test.source_config",
-        source_id={"a"},
-        target="test.target",
-        target_id={"b"},
-    )
-
-    # Missing source_id with target_id
-    with pytest.raises(ValueError):
-        Match(
-            cluster=1,
-            source="test.source_config",
-            target="test.target",
-            target_id={"b"},
-        )
-
-    # Missing cluster with target_id
-    with pytest.raises(ValueError):
-        Match(
-            source="test.source_config",
-            source_id={"a"},
-            target="test.target",
-            target_id={"b"},
-        )
-
-    # Missing source_id with cluster
-    with pytest.raises(ValueError):
-        Match(
-            cluster=1,
-            source="test.source_config",
-            target="test.target",
-        )
-
-
 @pytest.mark.parametrize(
     ("test_name", "should_work"),
     [
@@ -827,3 +787,43 @@ def test_source_integer_size_conversion(
         # Verify correct dtypes in the resulting DataFrame
         assert result.schema["small_int"] == small_int_type.to_dtype()
         assert result.schema["large_int"] == large_int_type.to_dtype()
+
+
+# Match
+
+
+def test_match_validates():
+    """Match objects are validated when they're instantiated."""
+    Match(
+        cluster=1,
+        source="test.source_config",
+        source_id={"a"},
+        target="test.target",
+        target_id={"b"},
+    )
+
+    # Missing source_id with target_id
+    with pytest.raises(ValueError):
+        Match(
+            cluster=1,
+            source="test.source_config",
+            target="test.target",
+            target_id={"b"},
+        )
+
+    # Missing cluster with target_id
+    with pytest.raises(ValueError):
+        Match(
+            source="test.source_config",
+            source_id={"a"},
+            target="test.target",
+            target_id={"b"},
+        )
+
+    # Missing source_id with cluster
+    with pytest.raises(ValueError):
+        Match(
+            cluster=1,
+            source="test.source_config",
+            target="test.target",
+        )
