d0e4706a17027d8a857b9cac5f71a1f1d3f972e4
Leonardo Mazzone <leonardo.mazzone@digital.trade.gov.uk>
2025-07-08 15:51:14 +0100

    Implement multi-user evaluation

--- src/matchbox/common/eval.py
+++ src/matchbox/common/eval.py
@@ -20,26 +20,55 @@ class Judgement(BaseModel):
     clusters: list[list[int]]
 
 
-def contains_to_pairs(contains: pl.DataFrame) -> Pairs:
-    """Convert clusters dataframe with parent, leaf columns to sets of pairs."""
-    pairs = set()
-    clusters = (
-        contains.group_by("parent")
-        .agg(pl.col("leaf").alias("leaves"))
-        .select("leaves")
-        .to_series()
-        .to_list()
-    )
-    for c in clusters:
-        pairs.update(combinations(sorted(c), r=2))
+def contains_to_pairs(contains: pl.DataFrame) -> dict[int, Pairs]:
+    """Convert clusters dataframe with parent, leaf columns to sets of pairs.
+
+    Args:
+        contains: A polars version of the judgements table produced by the server.
+
+    Returns:
+        A dict from user IDs to sets of pairs proposed, where pairs are all the possible
+        pair-wise edges between source clusters with the same parent according to the
+        user.
+    """
+    pairs: dict[int, Pairs] = {}
+    all_users = contains["user_id"].unique().to_list()
+    for user in all_users:
+        pairs[user] = set()
+
+        clusters = (
+            contains.filter(pl.col("user_id") == user)
+            .group_by("parent")
+            .agg(pl.col("child").alias("children"))
+            .select("children")
+            .to_series()
+            .to_list()
+        )
+
+        for c in clusters:
+            pairs[user].update(combinations(sorted(c), r=2))
 
     return pairs
 
 
-def precision_recall(model_pairs: Pairs, eval_pairs: Pairs) -> PrecisionRecall:
-    """Compute precision and recall scores."""
-    true_positives = len(eval_pairs.intersection(model_pairs))
-    precision = true_positives / len(model_pairs)
-    recall = true_positives / len(eval_pairs)
+def precision_recall(
+    model_pairs: Pairs, eval_pairs_by_user: dict[int, Pairs]
+) -> PrecisionRecall:
+    """Compute precision and recall scores, averaging across users."""
+    all_precision = []
+    all_recall = []
 
-    return round(precision, 3), round(recall, 3)
+    if not model_pairs or not eval_pairs_by_user:
+        raise ValueError("Both eval pairs and model pairs needed for precision-recall")
+
+    for user_pairs in eval_pairs_by_user.values():
+        if not user_pairs:
+            raise ValueError("Every user must have at least one pair")
+        true_positives = len(user_pairs.intersection(model_pairs))
+        all_precision.append(true_positives / len(model_pairs))
+        all_recall.append(true_positives / len(user_pairs))
+
+    def rounded_average(score: float) -> float:
+        return round(sum(score) / len(score), 3)
+
+    return rounded_average(all_precision), rounded_average(all_recall)
--- test/common/test_eval.py
+++ test/common/test_eval.py
@@ -1,56 +1,70 @@
 import polars as pl
 import pytest
 
+from matchbox.common.arrow import SCHEMA_JUDGEMENTS
 from matchbox.common.eval import Pairs, contains_to_pairs, precision_recall
 
 
 def test_contains_to_pairs():
     contains = pl.DataFrame(
         [
+            # -- First user --
             # Source clusters
-            [1, None],
-            [2, None],
-            [3, None],
+            [1, 1, None],
+            [1, 2, None],
+            [1, 3, None],
             # Model cluster
-            [4, 1],
-            [4, 2],
-            [4, 3],
+            [1, 4, 1],
+            [1, 4, 2],
+            [1, 4, 3],
             # Model cluster (order invariance)
-            [5, 3],
-            [5, 1],
-            [5, 2],
+            [1, 5, 3],
+            [1, 5, 1],
+            [1, 5, 2],
         ],
-        schema=["parent", "leaf"],
+        schema=SCHEMA_JUDGEMENTS.names,
     )
 
     expected_pairs = {(1, 2), (2, 3), (1, 3)}
 
-    pairs = contains_to_pairs(contains)
+    pairs_per_user = contains_to_pairs(contains)
 
-    assert pairs == expected_pairs
+    assert set(pairs_per_user.keys()) == {1}
+    assert pairs_per_user[1] == expected_pairs
 
 
 @pytest.mark.parametrize(
-    ["model_pairs", "eval_pairs", "prec", "recall"],
+    ["model_pairs", "eval_pairs_by_user", "prec", "recall"],
     [
         [
-            {(1, 2), (1, 3), (2, 3)},
-            {(1, 2)},
-            pytest.approx(1 / 3, 0.001),
-            1,
+            {(1, 2), (1, 3), (2, 3)},  # model
+            {1: {(1, 2)}},  # user
+            pytest.approx(1 / 3, 0.001),  # precision
+            1,  # recall
         ],
         [
-            {(1, 2)},
-            {(1, 2), (1, 3), (2, 3)},
-            1,
-            pytest.approx(1 / 3, 0.001),
+            {(1, 2)},  # model
+            {1: {(1, 2), (1, 3), (2, 3)}},  # user
+            1,  # precision
+            pytest.approx(1 / 3, 0.001),  # recall
+        ],
+        [
+            {(1, 2)},  # model
+            {
+                1: {(1, 2)},  # user 1
+                2: {(3, 4)},  # user 2
+            },
+            0.5,  # precision: mean of 1 and 0
+            0.5,  # recall: mean of 1 and 0
         ],
     ],
-    ids=["sub_precision", "sub_recall"],
+    ids=["sub_precision", "sub_recall", "multi_user"],
 )
 def test_precision_recall(
-    model_pairs: Pairs, eval_pairs: Pairs, prec: float, recall: float
+    model_pairs: Pairs, eval_pairs_by_user: dict[int, Pairs], prec: float, recall: float
 ):
-    p, r = precision_recall(model_pairs=model_pairs, eval_pairs=eval_pairs)
+    p, r = precision_recall(
+        model_pairs=model_pairs, eval_pairs_by_user=eval_pairs_by_user
+    )
     assert p == prec
     assert r == recall
