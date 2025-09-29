e962f7db4350b9d8380d3d0452294c0abc737622
Sergio Recio Rodriguez <sergio.reciorodriguez@digital.trade.gov.uk>
2025-09-29 15:49:32 +0100

    Update test_probabilities_to_results_entities to use Polars

--- test/common/factories/test_entity_factory.py
+++ test/common/factories/test_entity_factory.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import polars as pl
 import pyarrow as pa
 import pytest
 from faker import Faker
@@ -157,7 +158,7 @@ def test_generate_entities(features: tuple[FeatureConfig, ...], n: int):
     ),
     [
         pytest.param(
-            pa.table(
+            pl.DataFrame(
                 {
                     "left_id": [1, 2],
                     "right_id": [2, 3],
@@ -175,7 +176,7 @@ def test_generate_entities(features: tuple[FeatureConfig, ...], n: int):
             id="basic_dedupe_chain",
         ),
         pytest.param(
-            pa.table(
+            pl.DataFrame(
                 {
                     "left_id": [1],
                     "right_id": [4],
@@ -189,7 +190,7 @@ def test_generate_entities(features: tuple[FeatureConfig, ...], n: int):
             id="basic_link_match",
         ),
         pytest.param(
-            pa.table(
+            pl.DataFrame(
                 {
                     "left_id": [1, 2],
                     "right_id": [2, 3],
@@ -207,7 +208,7 @@ def test_generate_entities(features: tuple[FeatureConfig, ...], n: int):
             id="threshold_prevents_merge",
         ),
         pytest.param(
-            pa.table(
+            pl.DataFrame(
                 {
                     "left_id": [],
                     "right_id": [],
